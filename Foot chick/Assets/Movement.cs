75351dc4b753755e96e9f422f32ffd25c18d6f80
Adrián Jaime Pastor <36241023+darckball@users.noreply.github.com>
2019-06-10 02:24:25 +0200

    Finger control

---
+++ Foot chick/Assets/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Vector2 move, firstPos, endPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Control de movimiento del dedo sobre la pantalla
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                firstPos = touch.position;
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                endPos = touch.position;
+                move = endPos - firstPos;
+            }
+        }
+    }
+}
