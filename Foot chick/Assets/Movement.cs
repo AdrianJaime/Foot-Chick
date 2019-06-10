558b6779261150260f1e14f5b88c4e5d0db1413e
Adrián Jaime Pastor <36241023+darckball@users.noreply.github.com>
2019-06-10 03:18:38 +0200

    Movement done

--- Foot chick/Assets/Movement.cs
+++ Foot chick/Assets/Movement.cs
@@ -5,32 +5,43 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    public Vector2 move, firstPos, endPos;
+    public Vector3 firstCharacterPos, screenPos, move, firstFingerPos, endFingerPos;
 
+    public Camera cam;
+
+    public float dist;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dist = Mathf.Abs(gameObject.transform.position.z - cam.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         //Control de movimiento del dedo sobre la pantalla
         if(Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-
+            screenPos = cam.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, dist));
+            Debug.Log(screenPos);
             if (touch.phase == TouchPhase.Began)
             {
-                firstPos = touch.position;
+                firstFingerPos = screenPos;
+                firstCharacterPos = gameObject.transform.position;
+
             }
 
             if (touch.phase == TouchPhase.Moved)
             {
-                endPos = touch.position;
-                move = endPos - firstPos;
+                endFingerPos = screenPos;
+                move = endFingerPos - firstFingerPos;
+             
+                Debug.Log(firstFingerPos);
+                gameObject.transform.position = firstCharacterPos + new Vector3(move.x, move.y, gameObject.transform.position.z) ;
             }
         }
+
     }
 }
