b21e195c845652b5df71f6f9508cff3e6a208833
Adrián Jaime Pastor <36241023+darckball@users.noreply.github.com>
2019-06-19 21:35:26 +0200

    Texts

---
+++ Foot chick/Assets/Cinematics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cinematics : MonoBehaviour
+{
+    public GameObject[] text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("Tutorial", 0) == 0 && !text[0].activeSelf) text[0].SetActive(true);
+        else if(!text[1].activeSelf) text[1].SetActive(true);
+
+    }
+}
